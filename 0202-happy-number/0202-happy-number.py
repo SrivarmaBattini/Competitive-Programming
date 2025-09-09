a29a3054b8f72933d88aac572d1a902d27bb38bc
SrivarmaBattini <srivarma153@gmail.com>
2025-09-10 01:11:31 +0530

    Time: 1 ms (53.17%), Space: 17.7 MB (61.18%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,22 +1,8 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        ans = [n]
-        flag = True
-
-        while flag:
-            num = 0
-            while n != 0:
-                num += ((n % 10) ** 2)
-                n = n//10
-
-            if num == 1:
-                flag = False
-                break
-            n = num
-            
-            if n in ans:
-                break
-            else:
-                ans.append(n)
         
-        return not flag
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = sum(int(digit)**2 for digit in str(n))
+        return n == 1
