d6e7f074d27e4a0d50ad41baae31edc23227adf2
SrivarmaBattini <srivarma153@gmail.com>
2026-02-03 17:55:36 +0530

    Time: 0 ms (100%), Space: 19.5 MB (19.11%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isTrionic(self, nums: List[int]) -> bool:
+        
+        if len(nums) <= 3 or nums[1] < nums[0] or nums[-1] < nums[-2]:
+            return False
+        
+        p = len(nums)
+        for i in range(1,len(nums)):
+            if nums[i] > nums[i - 1]:
+                continue
+            elif nums[i] == nums[i - 1]:
+                return False
+            else:
+                p = i - 1
+                break
+        
+        if p == len(nums) or p == len(nums) - 1:
+            return False
+
+        q = len(nums)
+        for j in range(p+1, len(nums)):
+            if nums[j] < nums[j - 1]:
+                continue
+            elif nums[j] == nums[j - 1]:
+                return False
+            else:
+                q = j - 1
+                break
+        
+        def fun(ind):
+            nonlocal nums
+            for num in range(ind + 1, len(nums)):
+                if nums[num] <= nums[num - 1]:
+                    return False
+            return True
+            
+        return q < len(nums) - 1 and fun(q)
