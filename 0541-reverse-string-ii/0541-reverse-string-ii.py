6b51960947d0d8a6e6bacef6a8392f66839aa4f3
SrivarmaBattini <srivarma153@gmail.com>
2026-02-09 23:12:14 +0530

    Time: 7 ms (5.62%), Space: 19.6 MB (6.62%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -1,12 +1,32 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
         
-        res = []
-        n = len(s)
+        # res = []
+        # n = len(s)
 
-        for i in range(0, n, 2*k):
+        # for i in range(0, n, 2*k):
 
-            res.append(s[i:i+k][::-1])
-            res.append(s[i+k:i+2*k])
+        #     res.append(s[i:i+k][::-1])
+        #     res.append(s[i+k:i+2*k])
         
-        return "".join(res)
+        # return "".join(res)
+
+        def fun(ss):
+            
+            out = ""
+            m = min(k, len(ss))
+            for i in range(len(ss)):
+                if i < m:
+                    out += ss[m - i - 1]
+                else:
+                    out += ss[i]
+            return out
+
+        res = ""
+        for i in range(0, len(s), 2*k):
+            ns = fun(s[i:min(i+2*k,len(s))])
+            res += ns
+
+        return res
+
+        
