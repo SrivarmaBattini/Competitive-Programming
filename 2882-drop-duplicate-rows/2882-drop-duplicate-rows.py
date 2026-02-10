1d33216c9cb391d5255c02fa3b6e5ed63c415dc4
SrivarmaBattini <srivarma153@gmail.com>
2026-02-10 23:18:32 +0530

    Time: 276 ms (52.72%), Space: 67 MB (25.45%) - LeetHub

--- 2882-drop-duplicate-rows/2882-drop-duplicate-rows.py
+++ 2882-drop-duplicate-rows/2882-drop-duplicate-rows.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
-    return customers.drop_duplicates('email')
+    # return customers.drop_duplicates('email')
+    return customers.drop_duplicates(subset="email", keep="first")
