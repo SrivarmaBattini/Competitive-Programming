dca6fe4b1b6a7b732c2cc9305982b35c149b9278
SrivarmaBattini <srivarma153@gmail.com>
2026-03-01 09:42:07 +0530

    Time: 0 ms (100%), Space: 19.2 MB (94.18%) - LeetHub

--- 3856-trim-trailing-vowels/3856-trim-trailing-vowels.py
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.py
@@ -1,11 +1,9 @@
 class Solution:
     def trimTrailingVowels(self, s: str) -> str:
 
-        vowels = ['a','e','i','o','u']
-
         k = -1
         for i in range(len(s)):
-            if s[i] not in vowels:
+            if s[i] not in "aeiou":
                 k = i
         
         return s[:k+1]
