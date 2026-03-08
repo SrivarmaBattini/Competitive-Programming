83c2eede47cca985bdf28e62d59c234babc4b82d
SrivarmaBattini <srivarma153@gmail.com>
2026-03-08 11:37:39 +0530

    Time: 0 ms (100%), Space: 19.1 MB (99.21%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,27 +1,36 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         
-        def binary(n):
-            s = n[::-1]
-            val = 0
-            for i in range(len(n)):
-                val += int(s[i]) * (2 ** i)
-            return val
+        # def binary(n):
+        #     s = n[::-1]
+        #     val = 0
+        #     for i in range(len(n)):
+        #         val += int(s[i]) * (2 ** i)
+        #     return val
 
-        value = set()
-        for num in nums:
-            value.add(binary(num))
-        print(value)
-        i = len(nums)
-        for num in range(1, (2 ** i)):
-            if num not in value:
-                ans = ""
-                while num > 0:
-                    ans = str(num%2) + ans
-                    num = num // 2
+        # value = set()
+        # for num in nums:
+        #     value.add(binary(num))
+        # print(value)
+        # i = len(nums)
+        # for num in range(1, (2 ** i)):
+        #     if num not in value:
+        #         ans = ""
+        #         while num > 0:
+        #             ans = str(num%2) + ans
+        #             num = num // 2
                 
-                diff = len(nums) - len(ans)
-                pref = "0" * diff  
-                return pref + ans
+        #         diff = len(nums) - len(ans)
+        #         pref = "0" * diff  
+        #         return pref + ans
         
-        return "0" if nums[0] == "1" else "1"
+        # return "0" if nums[0] == "1" else "1"
+
+        val = ""
+        for i in range(len(nums)):
+            if nums[i][i] == "0":
+                val += "1"
+            else:
+                val += "0"
+        
+        return val
