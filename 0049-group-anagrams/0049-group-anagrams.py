82275d8977c3f78c43d90b15ca06ccd8fd196a2d
SrivarmaBattini <srivarma153@gmail.com>
2026-06-20 00:31:50 +0530

    Time: 17 ms (25.54%), Space: 21.9 MB (87.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,20 +2,31 @@ from collections import Counter
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
+        # group = {}
+
+        # for s in strs:
+            
+        #     hash = Counter(s)
+        #     print(hash)
+        #     sort_hash = dict(sorted(hash.items(), key = lambda x: x[0]))
+        #     ana = ""
+        #     for u, v in sort_hash.items():
+        #         ana += (str(u) + str(v))
+            
+        #     if ana in group:
+        #         group[ana].append(s)
+        #     else:
+        #         group[ana] = [s]
+        
+        # return [value for key, value in group.items()]
+
         group = {}
 
         for s in strs:
-            
-            hash = Counter(s)
-            print(hash)
-            sort_hash = dict(sorted(hash.items(), key = lambda x: x[0]))
-            ana = ""
-            for u, v in sort_hash.items():
-                ana += (str(u) + str(v))
-            
-            if ana in group:
-                group[ana].append(s)
+            ss = str(sorted(s))
+            if ss in group:
+                group[ss].append(s)
             else:
-                group[ana] = [s]
+                group[ss] = [s]
         
         return [value for key, value in group.items()]
