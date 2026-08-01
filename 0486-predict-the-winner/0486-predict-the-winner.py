aaba1bf8f921650dca884d431511d3caad6dcc64
SrivarmaBattini <srivarma153@gmail.com>
2026-08-02 00:08:21 +0530

    Time: 0 ms (100%), Space: 20.1 MB (11.54%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,17 @@
+from functools import lru_cache
+
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+
+        @lru_cache(None)
+        def dfs(i, j):
+
+            if i == j:
+                return nums[i]
+            
+            left = nums[i] - dfs(i + 1, j)
+            right = nums[j] - dfs(i, j - 1)
+
+            return max(left, right)
+        
+        return dfs(0, len(nums) - 1) >= 0
