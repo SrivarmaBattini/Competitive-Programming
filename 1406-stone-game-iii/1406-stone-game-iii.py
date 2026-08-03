63958bf38ca747eb52c8189b654b2fbcf1de2760
SrivarmaBattini <srivarma153@gmail.com>
2026-08-03 21:42:31 +0530

    Time: 1298 ms (27.77%), Space: 120.3 MB (37.99%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -0,0 +1,29 @@
+from functools import lru_cache
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        
+        n = len(stoneValue)
+
+        @lru_cache
+        def dfs(i):
+            if i >= n:
+                return 0
+
+            ans = float('-inf')
+            curr = 0
+
+            for k in range(3):
+                if i + k < n:
+                    curr += stoneValue[i+k]
+                    ans = max(ans, curr - dfs(i+k+1))
+
+            return ans
+        
+
+        res = dfs(0)
+        if res < 0:
+            return "Bob"
+        elif res == 0:
+            return "Tie"
+        else:
+            return "Alice"
