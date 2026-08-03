809393d628e2b3336c5baadf2ad09d7741a73221
SrivarmaBattini <srivarma153@gmail.com>
2026-08-03 23:15:46 +0530

    Time: 268 ms (19.25%), Space: 26.7 MB (20.97%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,41 @@
+from collections import deque
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        visited = set()
+        move = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        islands = 0
+
+        n = len(grid)
+        m = len(grid[0])
+
+
+        def bfs(sr, sc):
+            queue = deque([(sr, sc)])
+            visited.add((sr, sc))
+
+            while queue:
+                r, c = queue.popleft()
+
+                for dr, dc in move:
+                    nr = r + dr
+                    nc = c + dc
+                    
+                    if 0 <= nr < n and 0 <= nc < m:
+
+                        if (nr, nc) in visited:
+                            continue
+                        
+                        if grid[nr][nc] == "0":
+                            continue
+                        
+                        visited.add((nr, nc))
+                        queue.append((nr, nc))
+        
+        for sr in range(n):
+            for sc in range(m):
+                if grid[sr][sc] == "1" and (sr, sc) not in visited:
+                    islands += 1
+                    bfs(sr, sc)
+        
+        return islands
