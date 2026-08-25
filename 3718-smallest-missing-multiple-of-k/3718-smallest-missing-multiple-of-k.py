011f249d204a922dcd082a9d0c6ff278c2c9d4bc
SrivarmaBattini <srivarma153@gmail.com>
2026-08-25 23:44:16 +0530

    Time: 3 ms (20.84%), Space: 19.3 MB (18.61%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -4,9 +4,6 @@ class Solution:
         mini = min(nums)
         maxi = max(nums)
         maxi = maxi // k
-
-        if mini == 0:
-            mini = 1
         
         for i in range(1, maxi + 1):
             if i * k not in nums:
