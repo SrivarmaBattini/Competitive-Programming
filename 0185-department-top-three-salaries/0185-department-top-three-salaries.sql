dde483c8ddf95d88b1e8b536368020bf9a5d3af3
SrivarmaBattini <srivarma153@gmail.com>
2026-09-02 10:04:48 +0530

    Time: 967 ms (80.78%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -6,8 +6,8 @@ FROM (
            E.name AS Employee,
            E.salary AS Salary,
            DENSE_RANK() OVER(PARTITION BY D.id ORDER BY E.salary DESC) AS sr 
-           FROM Employee E 
-           LEFT JOIN Department D 
-           ON E.departmentId = D.id
+    FROM Employee E 
+    LEFT JOIN Department D 
+    ON E.departmentId = D.id
 ) t
 WHERE t.sr <= 3;
