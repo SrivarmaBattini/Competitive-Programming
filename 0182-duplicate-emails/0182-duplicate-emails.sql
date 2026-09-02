302036759912fe5222c06ee4d674447d8eec5170
SrivarmaBattini <srivarma153@gmail.com>
2026-09-02 08:59:25 +0530

    Time: 382 ms (78.06%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,8 +1,13 @@
 # Write your MySQL query statement below
 
-SELECT DISTINCT email AS Email 
-FROM Person P
-WHERE 1 < (
-    SELECT COUNT(M.email) FROM Person M 
-    WHERE M.email = P.email 
-);
+-- SELECT DISTINCT email AS Email 
+-- FROM Person P
+-- WHERE 1 < (
+--     SELECT COUNT(M.email) FROM Person M 
+--     WHERE M.email = P.email 
+-- );
+
+SELECT email AS Email 
+FROM Person
+GROUP BY email 
+HAVING COUNT(email) > 1;
