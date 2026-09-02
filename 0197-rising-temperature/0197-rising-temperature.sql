5e40297678ee3b26539c34481e266fdc69518591
SrivarmaBattini <srivarma153@gmail.com>
2026-09-02 23:08:31 +0530

    Time: 462 ms (79.38%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,10 +1,22 @@
 # Write your MySQL query statement below
 
-SELECT DISTINCT id FROM (
-    SELECT id, temperature, recordDate,
-        LAG(temperature) OVER(ORDER BY recordDate) AS prevTemp,
-        LAG(recordDate) OVER(ORDER BY recordDate) AS prevDate 
+-- SELECT DISTINCT id FROM (
+--     SELECT id, temperature, recordDate,
+--         LAG(temperature) OVER(ORDER BY recordDate) AS prevTemp,
+--         LAG(recordDate) OVER(ORDER BY recordDate) AS prevDate 
+--     FROM Weather
+-- ) t 
+-- WHERE DATEDIFF(recordDate, prevDate) = 1
+-- AND temperature > prevTemp;
+
+
+WITH CTE AS (
+    SELECT id, recordDate, temperature, 
+        LAG(temperature) OVER(ORDER BY recordDate) AS prevTemp, 
+        LAG(recordDate) OVER(ORDER BY recordDate) AS prevDate
     FROM Weather
-) t 
-WHERE DATEDIFF(recordDate, prevDate) = 1
-AND temperature > prevTemp;
+)
+
+SELECT id 
+FROM CTE WHERE temperature > prevTemp 
+AND DATEDIFF(recordDate, prevDate) = 1;
